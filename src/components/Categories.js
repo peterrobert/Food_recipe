0ab51e3c336dbc3db4f506e19174ea8aac7b4edf
peterrobert <pwambui93@gmail.com>
2021-02-10 13:06:28 +0300

    setup the first component

---
+++ src/components/Categories.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Categories extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+             hey this is the categories page
+            </div>
+        );
+    }
+}
+
+Categories.propTypes = {
+
+};
+
+export default Categories;
